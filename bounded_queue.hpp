3c8e6efa3bec08ff32d6436460384cfa1a5e4597
lpham259 <lucaspham59@gmail.com>
2025-10-05 16:13:38 -0500

    header file

---
+++ bounded_queue.hpp
@@ -0,0 +1,27 @@
+#ifndef BOUNDED_QUEUE_HPP
+#define BOUNDED_QUEUE_HPP
+
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+
+template <typename T>
+class BoundedQueue {
+private:
+    std::mutex mtx;
+    std::condition_variable not_full;
+    std::condition_variable not_empty;
+    std::queue<T> buffer;
+    size_t capacity;
+    bool closed = false;
+
+public:
+    explicit BoundedQueue(size_t cap);
+
+    void push(const T& item);
+    bool pop(T& item);
+    void close();
+};
+
+#include "bounded_queue.cpp"
+#endif
