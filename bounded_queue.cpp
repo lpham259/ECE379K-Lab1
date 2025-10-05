565fc2af043871910ebde787c86c1a99d3025a90
lpham259 <lucaspham59@gmail.com>
2025-10-05 17:06:06 -0500

    completed first iteration of implementation file

---
+++ bounded_queue.cpp
@@ -0,0 +1,48 @@
+#include "bounded_queue.hpp"
+
+template <typename T>
+BoundedQueue<T>::BoundedQueue(size_t cap) : capacity(cap) {}
+
+template <typename T>
+void BoundedQueue<T>::push(const T& item) {
+    std::unique_lock<std::mutex> lock(mtx);
+
+    // Wait while buffer is full and not closed
+    while(buffer.size() == capacity && !closed) {
+        not_full.wait(lock);
+    }
+
+    if(closed) {
+        throw std::runtime_error("Queue is closed");
+    }
+
+    buffer.push(item);
+    not_empty.notify_one();
+}
+
+template <typename T>
+bool BoundedQueue<T>::pop(T& item) {
+    std::unique_lock<std::mutex> lock(mtx);
+
+    // Wait while buffer is empty and not closed
+    while(buffer.empty() && !closed) {
+        not_empty.wait(lock);
+    }
+
+    if buffer(.empty()) {
+        return false;
+    }
+
+    item = buffer.front();
+    buffer.pop();
+    not_full.notify_one();
+    return true;
+}
+
+template <typename T>
+void BoundedQueue<T>::close() {
+    std::unique_lock<std::mutex> lock(mtx);
+    closed = true;
+    not_full.notify_all();
+    not_empty.notify_all();
+}
